c917fe703e523c1c595fb2e759998b8e079e489d
Nixy <kpoleg2@gmail.com>
2014-01-24 18:54:23 +0400

    Signed-off-by: Nixy <kpoleg2@gmail.com>

--- src/com/comfymobile/saadat/adapter/RSSReader.java
+++ src/com/comfymobile/saadat/adapter/RSSReader.java
@@ -4,20 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-<<<<<<< HEAD
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import com.comfymobile.saadat.R;
-=======
->>>>>>> origin/master
 import com.comfymobile.saadat.activity.MenuActivity;
 import com.comfymobile.saadat.database.LocalDatabase;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
