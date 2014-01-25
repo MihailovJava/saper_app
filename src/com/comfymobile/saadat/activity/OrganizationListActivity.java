25bb22ed6cbe43f4dac81aaa9af6e4824d95f6a0
Nixy <kpoleg2@gmail.com>
2014-01-26 00:17:15 +0400

    Signed-off-by: Nixy <kpoleg2@gmail.com>

--- src/com/comfymobile/saadat/activity/OrganizationListActivity.java
+++ src/com/comfymobile/saadat/activity/OrganizationListActivity.java
@@ -79,4 +79,16 @@ public class OrganizationListActivity extends SherlockActivity {
         });
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EasyTracker.getInstance(this).activityStart(this);  // Add this method.
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EasyTracker.getInstance(this).activityStop(this);  // Add this method.
+    }
 }
